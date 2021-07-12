b341eecf9ba4931f0b288967071f87d904a30c4a
phillevn <35641651+phillevn@users.noreply.github.com>
2021-07-11 20:59:11 -0500

    Update fgwas_pbs.sh

--- src_code/fgwas_pbs.sh
+++ src_code/fgwas_pbs.sh
@@ -1,6 +1,6 @@
 dat_DIR="/ddn/home2/r2725/projects/jeannette/data/"
 
-for i in {1..1};
+for i in {1..22};
         do
                 cd ${dat_DIR}chr${i}_data/subset_chro${i}_AA_filtered
                         for string in *.pbs;
